ac7054b80d30dcde2667e42a596af9d52b6b17b9
Ansh Gupta <creatorlogs.official@gmail.com>
2024-07-11 13:29:54 +0530

    made some changes

--- backend/app.py
+++ backend/app.py
@@ -44,4 +44,4 @@ def predict(input_data: InputData):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
